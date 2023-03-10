93c7327a104970d3e65e33409e5ba55b70e1c767
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2023-03-10 16:34:25 +0900

    rtd

---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'py2shpss'
+copyright = '2020-, Hideyuki Tachibana'
+author = 'Hideyuki Tachibana'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    'autoapi.extension',
+]
+
+autodoc_default_flags = [
+   'members',
+   'private-members'
+]
+
+autodoc_mock_imports = ['py2shpss']
+
+autoapi_type = 'python'
+autoapi_dirs = ['../py2shpss']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. py2shpss documentation master file, created by
+   sphinx-quickstart on Fri Mar 10 01:56:15 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to py2shpss's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. automodule:: py2shpss
+   :members:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
